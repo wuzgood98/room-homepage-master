dc8f6dcf60db6a1f8b7667d53b1b8757c49fa2ac
Gideon Addo <gideonaddoofori@gmail.com>
2022-11-25 11:59:09 +0000

    Refactor gallery data array

--- src/data/data.js
+++ src/data/data.js
@@ -7,10 +7,11 @@ import desktopThree from "../assets/images/desktop-image-hero-3.jpg";
 
 const randomNumber = () => Math.floor(Math.random() * 100000);
 
-const mobile = [
+const galleryData = [
   {
     id: randomNumber(),
-    img: mobileOne,
+    mobileImg: mobileOne,
+    desktopImg: desktopOne,
     title: "Discover innovative ways to decorate",
     content:
       "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.",
@@ -18,7 +19,8 @@ const mobile = [
   },
   {
     id: randomNumber(),
-    img: mobileTwo,
+    mobileImg: mobileTwo,
+    desktopImg: desktopTwo,
     title: "We are available all across the globe",
     content:
       "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country.Find the branch nearest you using our store locator.Any questions? Don't hesitate to contact us today.",
@@ -26,7 +28,8 @@ const mobile = [
   },
   {
     id: randomNumber(),
-    img: mobileThree,
+    mobileImg: mobileThree,
+    desktopImg: desktopThree,
     title: "Manufactured with the best materials",
     content:
       "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible.With three decades of experience in this industry, we understand what customers want for their home and office.",
@@ -34,31 +37,4 @@ const mobile = [
   },
 ];
 
-const desktop = [
-  {
-    id: randomNumber(),
-    img: desktopOne,
-    title: "Discover innovative ways to decorate",
-    content:
-      "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-    action: "Shop now",
-  },
-  {
-    id: randomNumber(),
-    img: desktopTwo,
-    title: "We are available all across the globe",
-    content:
-      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country.Find the branch nearest you using our store locator.Any questions? Don't hesitate to contact us today.",
-    action: "Shop now",
-  },
-  {
-    id: randomNumber(),
-    img: desktopThree,
-    title: "Manufactured with the best materials",
-    content:
-      "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible.With three decades of experience in this industry, we understand what customers want for their home and office.",
-    action: "Shop now",
-  },
-];
-
-export { mobile, desktop };
+export default galleryData;
